ef0709c0eea56e1ef5861048ee829b0c0d46214b
MohammadYounoos <114152824+MohammadYounoos@users.noreply.github.com>
2023-02-02 10:04:03 +0530

    Add files via upload

---
+++ simple calculator.python.py
@@ -0,0 +1,14 @@
+m=int(input())
+print("m=",m)
+n=int(input())
+print("n=",n)
+def cal():
+    s=m+n
+    sub=m-n
+    mul=m*n
+    div=m/n
+    print("add:",s)
+    print("sub:",sub)
+    print("mul:",mul)
+    print("div:",div)
+print("cal: ",cal())
